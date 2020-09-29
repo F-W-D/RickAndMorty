34a64380c2db742c5a7f0a3df7354f6cbf4ea965
F-W-D <pj_gutierrez@outlook.com>
2020-09-29 17:03:33 -0600

    Implemented Character ViewModel into the Content view

--- Rick and Morty/ContentView.swift
+++ Rick and Morty/ContentView.swift
@@ -9,72 +9,71 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    
+    @State private var selectedCharacter: Character?
+    @State private var showingAlert = false
+    
+    @ObservedObject var charactersVM = CharactersVM()
 
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        NavigationView {
+            List {
+                ForEach(charactersVM.characters) { character in
+                    HStack {
+                        Button(action: {
+                            self.selectedCharacter = character
+                            self.showingAlert = true
+                        }) {
+                            Text(character.name)
+                        }
+                        
+                        Spacer()
+                        
+                        VStack(alignment: .trailing) {
+                            Text("Status: " + character.status).fontWeight(.light).font(.system(size: 12))
+                            Text("Species: " + character.species).fontWeight(.light).font(.system(size: 12))
+                        }
+                    }
+                }
+                
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
+            .cornerRadius(10.0)
+            .navigationBarTitle("Rick & Morty")
+            .navigationBarItems(
+                leading:
+                    Button(action: {
+                        loadPage(action: .previous)
+                    }, label: {
+                        Label("", systemImage: "chevron.left.circle")
+                    }),
+                trailing:
+                    Button(action: {
+                        loadPage(action: .next)
+                    }, label: {
+                        Label("", systemImage: "chevron.right.circle")
+                    })
+            )
+        }.alert(isPresented: $showingAlert) {
+            
+            let name = selectedCharacter?.name ?? ""
+            let locationName = selectedCharacter?.getLocationName() ?? ""
+            
+            return Alert(title: Text(name),
+                  message: Text("This character is located on \(locationName)"),
+                  dismissButton: .default(Text("Got it!")))
         }
     }
+    
 
-    private func addItem() {
+    private func loadPage(action: CharactersPageAction) {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            charactersVM.getCharacters(action: action)
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
